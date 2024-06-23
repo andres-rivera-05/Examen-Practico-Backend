a72f94068049854c851b6d95fd26aeb0867371c1
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-06-22 22:29:49 -0600

    ultimo commit

--- db/database_examen.sql
+++ db/database_examen.sql
@@ -4,20 +4,8 @@ CREATE TABLE Items (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     descripcion VARCHAR(255) NOT NULL,
-    estado BOOLEAN DEFAULT True,
+    estado VARCHAR(255) NOT NULL,
     categoria VARCHAR(255) NOT NULL,
     precio INTEGER NOT NULL,
     imagen bytea
 );
-
-CREATE TABLE Categoria(
-  id SERIAL PRIMARY KEY,
-  nombre VARCHAR(255)
-)
-
-INSERT INTO Categoria(nombre)VALUES('Herramientas');
-
-SELECT * FROM categoria
-
-INSERT INTO Items(nombre, descripcion, estado, categoria, precio, imagen)
-VALUES ('tasa', 'tasa color negro para tomar cafe', True, 'Utensilios', 24, NULL )
--- index.js
+++ index.js
@@ -2,15 +2,12 @@ import express from 'express';
 import multer from 'multer';
 import { db } from './db/conn.js';
 import cors from 'cors';
-
 const app = express();
 const PORT = 4756;
-
-// Configuración de multer para almacenar archivos en memoria
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage: storage })
 
-// Middleware para parsear JSON
+
 app.use(express.json());
 
 const corsOptions = {
@@ -22,75 +19,58 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-// Endpoint para obtener todos los items
-app.get('/', async (req, res) => {
+
+app.get('', async (req, res) => {
   try {
+    const { id } = req.params;
     const sql = `SELECT * FROM Items`;
-    const result = await db.query(sql);
-    res.json(result.rows);
+    const result = await db.query(sql, [id]);
+    res.json(result);
   } catch (err) {
-    console.error(err.message);
     res.status(500).json({ mensaje: err.message });
   }
 });
 
-// Endpoint para obtener un item por id
+
 app.get('/item/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const sql = `SELECT * FROM Items WHERE id = $1;`;
+    const sql = `SELECT * FROM Items WHERE id = $1`;
     const result = await db.query(sql, [id]);
-    res.json(result.rows[0]);
+    res.json(result);
   } catch (err) {
-    console.error(err.message);
     res.status(500).json({ mensaje: err.message });
   }
 });
 
-// Endpoint para crear un nuevo item
 app.post('/', upload.single('imagen'), async (req, res) => {
   try {
     const { nombre, descripcion, estado, categoria, precio } = req.body;
-    let imagen = null;
-
-    console.log('Body:', req.body); // Verifica qué está llegando en el cuerpo de la solicitud
-
-    if (req.file) {
-      // Obtener el buffer de la imagen
-      imagen = req.file.buffer;
-
-      // Verificar el tamaño y tipo de la imagen
-      console.log('Tamaño de la imagen:', imagen.length);
-      console.log('Tipo de la imagen:', req.file.mimetype);
-    } else {
-      console.log('No se recibió imagen');
-    }
-
-    const params = [nombre, descripcion, estado === 'true', categoria, parseInt(precio), imagen];
+    const imagen = req.file ? req.file.buffer.toString('base64') : null;
+    const params = [nombre, descripcion, estado, categoria, precio, imagen];
     const sql = `INSERT INTO Items
-                (nombre, descripcion, estado, categoria, precio, imagen)
-                VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+    (nombre, descripcion, estado, categoria, precio, imagen)
+    VALUES($1, $2, $3, $4, $5, $6) RETURNING id, 'Insertado con éxito' as mensaje`;
+
     const result = await db.query(sql, params);
-    res.json(result.rows[0]);
+    res.json(result);
   } catch (err) {
-    console.error(err.message);
+    console.error(err.message); 
     res.status(500).json({ mensaje: err.message });
   }
 });
 
-// Endpoint para eliminar un item por id
 app.delete('/item/delete/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const sql = `DELETE FROM Items WHERE id = $1 RETURNING 'Item eliminado con exito' mensaje;`;
+    const sql = `DELETE FROM Items WHERE id = $1 RETURNING 'Item eliminado con éxito' as mensaje`;
     const result = await db.query(sql, [id]);
-    res.json(result.rows[0]);
+    res.json(result);
   } catch (err) {
-    console.error(err.message);
     res.status(500).json({ mensaje: err.message });
   }
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto: ${PORT}`);
+  console.log(`Servidor escuchando en el puerto: ${ PORT }`);
 });
